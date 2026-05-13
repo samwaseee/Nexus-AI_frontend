b1b31fee9dc247f56bd157a8f98cade26c4a9806
samwaseee <samwaseee@gmail.com>
2026-05-13 19:11:18 +0600

    feat(home): remove Navbar and Footer imports from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Navbar } from "@/components/layout/Navbar";
-import { Footer } from "@/components/layout/Footer";
 import { HeroSection } from "@/components/home/HeroSection";
 import { FeaturesSection } from "@/components/home/FeaturesSection";
 import { AIShowcase } from "@/components/home/AIShowcase";
